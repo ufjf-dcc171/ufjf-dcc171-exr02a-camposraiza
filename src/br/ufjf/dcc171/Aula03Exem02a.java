b50dc1285f6cab72e7948f5c9adedb0ceb68bbca
raiza raizacampos2@gmail.com <>
2017-08-22 21:12:13 -0300

    teste

---
+++ src/br/ufjf/dcc171/Aula03Exem02a.java
@@ -0,0 +1,17 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula03Exem02a {
+
+   public static void main(String[] args) {
+       
+    JanelaSoma janela = new JanelaSoma(); 
+    janela.setSize(300,200);
+    janela.setLocationRelativeTo(null);
+    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    janela.setVisible(true);
+       
+   }
+    
+}
